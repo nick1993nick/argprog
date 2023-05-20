0e00680dea4fb8633a04a9bd5cab8a577bc63510
nick1993nick <nico-g93@outlook.com>
2023-05-20 12:02:28 -0300

    mejoras

--- practico0/promedio.js
+++ practico0/promedio.js
@@ -1,6 +1,8 @@
 let numero1 = 23;
 let numero2 = 53;
 let numero3 = 100;
-let promedio = (numero1 + numero2 + numero3) / 3;
+let numero4 = 300;
+let numero5 = 67
+let promedio = (numero1 + numero2 + numero3 + numero4 + numero5) / 5;
 console.log(promedio);
-console.log(hola);
+
